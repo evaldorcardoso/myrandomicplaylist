29f7c62edf775659b658990bace112b9a4d0e4dc
Evaldo Cardoso <evaldorcardoso@outlook.com>
2023-06-04 16:36:06 -0300

    fix: icons for pwa

--- vite.config.js
+++ vite.config.js
@@ -19,17 +19,17 @@ export default defineConfig({
       theme_color: '#62faf5',
       icons: [
         {
-          src: 'android-icon-192x192.png',
+          src: 'assets/android-icon-192x192.png',
           sizes: '192x192',
           type: 'image/png'
         },
         {
-          src: 'mrp-logo-512x512.png',
+          src: 'assets/mrp-logo-512x512.png',
           sizes: '512x512',
           type: 'image/png'
         },
         {
-          src: 'mrp-logo-512x512.png',
+          src: 'assets/mrp-logo-512x512.png',
           sizes: '512x512',
           type: 'image/png',
           purpose: 'any maskable'
