90b6fada24e89af2706f50929c7172b313c82371
Evaldo Cardoso <evaldorcardoso@outlook.com>
2023-06-07 18:22:14 -0300

    fix: vite pwa fix

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  assetsInclude: ['**/*.png'],
   plugins: [vue(), loadVersion(), VitePWA({ 
     registerType: 'autoUpdate',
     devOptions: {
